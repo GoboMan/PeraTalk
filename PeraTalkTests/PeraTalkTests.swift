09f62d0a70523d34d61ab7786f1904d141079449
GoboMan <hagegobou@icloud.com>
2026-05-02 13:18:32 +0900

    Initial Commit

---
+++ PeraTalkTests/PeraTalkTests.swift
@@ -0,0 +1,19 @@
+//
+//  PeraTalkTests.swift
+//  PeraTalkTests
+//
+//  Created by Taisei Abe on 2026/05/02.
+//
+
+import Testing
+@testable import PeraTalk
+
+struct PeraTalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
